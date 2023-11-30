c77b54c82bf4ae566783d3df61991adaa74c7bbf
Isa Vega <imvega100@gmail.com>
2023-11-30 14:58:54 -0500

    thought and reaction schema done

---
+++ models/Thought.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create Post model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //formats date to mm/dd/yyyy
+      get:(newDate)=> newDate.toLocaleDateString()
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `responses` that gets the amount of response per video
+thoughtSchema
+  .virtual('reactionCount')
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our Video model
+const Thought = model('thouhgt', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -39,17 +39,11 @@ const userSchema = new Schema(
 
 // Create a virtual property `fullName` that gets and sets the user's full name
 userSchema
-  .virtual('fullName')
+  .virtual('friendCount')
   // Getter
   .get(function () {
-    return `${this.first} ${this.last}`;
+    return `${this.friends.length}`;
   })
-  // Setter to set the first and last name
-  .set(function (v) {
-    const first = v.split(' ')[0];
-    const last = v.split(' ')[1];
-    this.set({ first, last });
-  });
 
 // Initialize our User model
 const User = model('user', userSchema);
