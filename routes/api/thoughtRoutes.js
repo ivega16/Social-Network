b8281434375c2c1aa7ad4240cd8b254ef1341254
Isa Vega <imvega100@gmail.com>
2023-11-30 16:01:39 -0500

    fixed controllers and api routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addThoughtResponse,
+  removeThoughtResponse,
+} = require('../../controllers/thoughtController');
+
+// /api/Thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/Thoughts/:ThoughtId
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/Thoughts/:ThoughtId/responses
+router.route('/:thoughtId/responses').post(addThoughtResponse);
+
+// /api/Thoughts/:ThoughtId/responses/:responseId
+router.route('/:ThoughtId/responses/:responseId').delete(removeThoughtResponse);
+
+module.exports = router;
